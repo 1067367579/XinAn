6498b2b21047e8409765a3fce44131985f031de1
steve-lu <1067367579@qq.com>
2024-03-30 16:46:11 +0800

    ProcessController

---
+++ XinAn-server/src/main/java/com/xinan/controller/ProcessController.java
@@ -0,0 +1,41 @@
+package com.xinan.controller;
+
+import com.xinan.result.Result;
+import com.xinan.vo.ProcessVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/process")
+@Api(tags = "服务模块相关接口")
+@Slf4j
+public class ProcessController {
+
+    //查看服务进度 按照用户查询
+    @GetMapping("/{userId}")
+    @ApiOperation(value = "按照用户id查询服务流程")
+    public Result<ProcessVO> getByUserId(@PathVariable Long userId)
+    {
+        log.info("按照用户id查询服务流程:{}",userId);
+
+        return Result.success();
+    }
+
+    //修改服务流程 添加 删除(批量删除)
+    @DeleteMapping("/{ids}")
+    @ApiModelProperty(value = "按照流程id批量删除流程")
+    public Result deleteByIds(@PathVariable List<Long> ids)
+    {
+        return Result.success();
+    }
+
+    //条件查询商家
+
+    //流程细则?
+
+}
